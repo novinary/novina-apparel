c127a7fb07e8fb8939d61c003eb42a0ac2c6685b
novinary <novina.dev@gmail.com>
2019-09-11 20:22:56 +0100

    Update menu-items to navigate withRouter HOC

--- novina-apparel/src/App.js
+++ novina-apparel/src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages-components/homepage/homepage.component';
 
+const HatsPage = () => (
+  <div>
+    <h1>THIS IS HATS PAGE</h1>
+  </div>
+);
+
 function App() {
-  return <HomePage />;
+  return (
+    <Switch>
+      <Route exact path='/' component={HomePage} />
+      <Route path='/hats' component={HatsPage} />
+    </Switch>
+  );
 }
 
 export default App;
