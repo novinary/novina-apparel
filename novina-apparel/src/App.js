96861e3fa67c187945eb6a7652d20dbda78cb5de
novinary <novina.dev@gmail.com>
2019-09-12 04:34:32 +0100

    Implement firebase open and close subscription to handle auth changes on firebase

--- novina-apparel/src/App.js
+++ novina-apparel/src/App.js
@@ -6,18 +6,47 @@ import HomePage from './pages-components/homepage/homepage.component';
 import ShopPage from './pages-components/shop/shop.component';
 import Header from './reuseable-components/header/header.component';
 import SignInAndSignUpPage from './pages-components/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import { auth } from './firebase/firebase.utils';
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <Switch>
-        <Route exact path='/' component={HomePage} />
-        <Route path='/shop' component={ShopPage} />
-        <Route path='/signin' component={SignInAndSignUpPage} />
-      </Switch>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      currentUser: null
+    };
+  }
+
+  unsubscribeFromAuth = null;
+
+  componentDidMount() {
+    // open subscription which means connection is always open
+    // as long as out app component is mounted on our dom
+    // but because it's an open subscription we also have
+    // to close subscription(unsubscribeFromAuth) when
+    // this unmounts to prevent memory leaks in our js app
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({ currentUser: user });
+    });
+  }
+
+  // close subscription whenever our component unmounts
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+
+  render() {
+    return (
+      <div>
+        <Header />
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route path='/shop' component={ShopPage} />
+          <Route path='/signin' component={SignInAndSignUpPage} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
