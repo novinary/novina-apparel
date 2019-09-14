4eb07b04a6a36d78f78b68c3d27a0f01fee70360
novinary <novina.dev@gmail.com>
2019-09-14 01:44:33 +0100

    Store user data in Firebase and in the state of my App so I can use it in my app

--- novina-apparel/src/App.js
+++ novina-apparel/src/App.js
@@ -6,7 +6,7 @@ import HomePage from './pages-components/homepage/homepage.component';
 import ShopPage from './pages-components/shop/shop.component';
 import Header from './reuseable-components/header/header.component';
 import SignInAndSignUpPage from './pages-components/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 class App extends React.Component {
   constructor() {
@@ -25,8 +25,22 @@ class App extends React.Component {
     // but because it's an open subscription we also have
     // to close subscription(unsubscribeFromAuth) when
     // this unmounts to prevent memory leaks in our js app
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user });
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          });
+
+          console.log(this.state);
+        });
+      }
+      this.setState({ currentUser: userAuth });
     });
   }
 
