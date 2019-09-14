d4807f3216e05efce4a892901bfdedfc9f8ab36e
novinary <novina.dev@gmail.com>
2019-09-14 02:48:12 +0100

    Bring SingUp into SignInAndSignUpPage

--- novina-apparel/src/pages-components/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ novina-apparel/src/pages-components/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -2,9 +2,12 @@ import React from 'react';
 import './sign-in-and-sign-up.styles.scss';
 import SignIn from '../../reuseable-components/sign-in/sign-in.component';
 
+import SignUp from '../../reuseable-components/sign-up/sign-up.component';
+
 const SignInAndSignUpPage = () => (
   <div className='sign-in-and-sign-up'>
     <SignIn />
+    <SignUp />
   </div>
 );
 
