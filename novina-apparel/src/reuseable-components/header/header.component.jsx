5d511c42e77a6a1f01eea736c7769e42f462eaae
novinary <novina.dev@gmail.com>
2019-12-03 02:34:34 +0000

    wip

--- novina-apparel/src/reuseable-components/header/header.component.jsx
+++ novina-apparel/src/reuseable-components/header/header.component.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import { auth } from '../../firebase/firebase.utils';
+import CartIcon from '../cart-icon/cart-icon.component';
 
 import { ReactComponent as Logo } from '../../assets/logo.svg';
 import './header.styles.scss';
@@ -28,6 +29,7 @@ const Header = ({ currentUser }) => (
           SIGN IN
         </Link>
       )}
+      <CartIcon />
     </div>
   </div>
 );
