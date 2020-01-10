d97def295ed3c0e6b1fef7a5cc1812650a8c5b2e
novinary <novina.dev@gmail.com>
2020-01-10 15:33:54 +0000

    Made some cosmetic changes

--- novina-apparel/src/redux/directory/directory.reducer.js
+++ novina-apparel/src/redux/directory/directory.reducer.js
@@ -2,7 +2,7 @@ const INITIAL_STATE = {
   sections: [
     {
       title: 'hats',
-      imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+      imageUrl: 'https://i.ibb.co/QbXJ0kY/hats.png',
       id: 1,
       linkUrl: 'shop/hats'
     },
@@ -13,21 +13,21 @@ const INITIAL_STATE = {
       linkUrl: 'shop/jackets'
     },
     {
-      title: 'sneakers',
-      imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+      title: 'trainers',
+      imageUrl: 'https://i.ibb.co/gTXZc25/trainers.jpg',
       id: 3,
-      linkUrl: 'shop/sneakers'
+      linkUrl: 'shop/trainers'
     },
     {
       title: 'womens',
-      imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+      imageUrl: 'https://i.ibb.co/rGHgXTj/womens.jpg',
       size: 'large',
       id: 4,
       linkUrl: 'shop/womens'
     },
     {
       title: 'mens',
-      imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+      imageUrl: 'https://i.ibb.co/jW4gHCJ/men.png',
       size: 'large',
       id: 5,
       linkUrl: 'shop/mens'
